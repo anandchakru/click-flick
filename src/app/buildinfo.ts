c3e5c6a134f2a134770656cd8b00cc703f48a049
Anand <anandchakru@gmail.com>
2023-04-01 11:24:14 -0700

    lint issue fix

---
+++ src/app/buildinfo.ts
@@ -0,0 +1,12 @@
+// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* tslint:disable */
+export const BUILDINFO = {
+  "hash": "localhostversion",
+  "tag": "",
+  "github": {
+    "runId": "",
+    "runNumber": "",
+    "runAttempt": ""
+  }
+};
+/* tslint:enable */
